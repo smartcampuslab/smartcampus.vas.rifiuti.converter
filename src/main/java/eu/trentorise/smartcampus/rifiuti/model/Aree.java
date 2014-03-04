731225882fb9f89376690f66a27cd9f4b632cea0
gioadami <giordano.adami@gmail.com>
2014-03-04 10:17:28 +0100

    Added comune/via/numero to Aree.

--- src/main/java/eu/trentorise/smartcampus/rifiuti/model/Aree.java
+++ src/main/java/eu/trentorise/smartcampus/rifiuti/model/Aree.java
@@ -9,6 +9,9 @@ public class Aree {
 	private String parent;
 	private String gestore;
 	private String descrizione;
+	private String comune;
+	private String via;
+	private String numero;
 	
 	public String getIstituzione() {
 		return istituzione;
@@ -48,6 +51,30 @@ public class Aree {
 
 	public void setDescrizione(String descrizione) {
 		this.descrizione = descrizione;
+	}
+
+	public String getComune() {
+		return comune;
+	}
+
+	public void setComune(String comune) {
+		this.comune = comune;
+	}
+
+	public String getVia() {
+		return via;
+	}
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
 	}
 
 }
