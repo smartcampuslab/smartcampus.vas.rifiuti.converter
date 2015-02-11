410c216727e9b8153e326be11a297805f3121187
raman <raman@fbk.eu>
2015-02-11 11:37:41 +0100

    dati version 26

--- src/main/java/eu/trentorise/smartcampus/rifiuti/Main.java
+++ src/main/java/eu/trentorise/smartcampus/rifiuti/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 		Initializer initializer = new Initializer();
 
-		Rifiuti rifiuti = initializer.readExcel("ExcelModelloConcettuale_V0.24.xls");
+		Rifiuti rifiuti = initializer.readExcel("ExcelModelloConcettuale_V0.26.xls");
 		initializer.writeCSV(rifiuti);
 		initializer.convertFromCSVToDB();
 		
